bb41991b11d6f240e4873b726ea321b2455f8244
ashupal <ashupal0005@gmail.com>
2018-07-30 15:19:38 +0530

    update changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,6 +52,16 @@ function getNumber(num){
     }
 }
 
+function leftBracket(){
+    var_num = document.getElementById('display-value');
+    // var bracket = var_num.value;
+    var_num.value += '(';
+}
+
+function rightBracket(){
+    var_num = document.getElementById('display-value');
+    var_num.value += ')';
+}
 
 // clear the screen
 function CE(){
@@ -131,7 +141,8 @@ function half(){
 
 function square(){
     var_num = document.getElementById('display-value');
-    var square = var_num.value * var_num.value;
+    // var square = var_num.value * var_num.value;
+    var square = Math.pow(var_num.value,2);
     var_num.value = square;
 }
 
@@ -139,4 +150,133 @@ function sqrt(){
     var_num = document.getElementById('display-value');
     var sqrt = Math.sqrt(var_num.value);
     var_num.value = sqrt;
+}
+
+
+// function memoryPlus(){
+//   var_num = document.getElementById('display-value');
+//   if(var_num.value == ''){
+//       return
+     
+//   }
+//   var_num.value += "+" + var_num.value;
+//  var_num.value 
+// }
+
+
+
+
+
+function pi_func(){
+    var_num = document.getElementById('display-value');
+    console.log(Math.PI);
+    
+    var pi = Math.PI;
+    var_num.value = pi;
+}
+
+function Exp(){
+    var_num = document.getElementById('display-value');
+    var exp = Math.exp(var_num.value);
+    var_num.value = exp;
+}
+
+function sin(){
+    var_num = document.getElementById('display-value');
+    var sin = Math.sin(var_num.value);
+    var_num.value = sin;
+}
+function cos(){
+    var_num = document.getElementById('display-value');
+    var cos = Math.cos(var_num.value);
+    var_num.value = cos;
+}
+function tan(){
+    var_num = document.getElementById('display-value');
+    var tan = Math.tan(var_num.value);
+    var_num.value = tan;
+}
+
+function log(){
+    var_num = document.getElementById('display-value');
+    var log = Math.log(var_num.value);
+    var_num.value = log;
+}
+
+
+function power(){
+    var_num = document.getElementById('display-value');
+    var ans=var_num.value, i;
+    for(i=1; i < var_num.value; i++){
+        ans = ans * var_num.value;
+      
+    }
+    var_num.value = ans;
+}
+
+
+function powerValue(){
+    var_num = document.getElementById('display-value');
+    var ans = Math.pow(10, var_num.value);
+    var_num.value = ans;
+}
+
+
+// function modulus(){
+//     var_num = document.getElementById('display-value');
+//     var ans = Math.mod(var_num.value);
+//     var_num.value = ans;
+// }
+  
+
+function factorial(){
+    var_num = document.getElementById('display-value');
+    var i, fact = 1;
+    for(i=1;i<=var_num.value;i++){
+        fact = fact * i;
+    }
+    var_num.value = fact;
+}
+
+
+
+function previous(){
+    var prev = document.getElementById('maths-func1');
+    var next = document.getElementById('maths-func2');
+    if(prev.style.display === "block" || next.style.display === "none"){
+        prev.style.display = "none";
+        next.style.display = "block";
+    }
+    else
+    {
+        prev.style.display = "block";
+        next.style.display = "none";
+    }
+ 
+}
+
+function cube(){
+    var_num = document.getElementById('display-value');
+   
+    var cube = Math.pow(var_num.value,3);
+    var_num.value = cube;
+}
+
+
+function sinInv(){
+    var_num = document.getElementById('display-value');
+    var sinInv = Math.asin(var_num.value);
+    var_num.value = sinInv;
+}
+
+function CosInv(){
+    var_num = document.getElementById('display-value');
+    var cosInv = Math.acos(var_num.value);
+    var_num.value = cosInv;
+}
+
+function tanInv(){
+    var_num = document.getElementById('display-value');
+    var tanInv = Math.atan(var_num.value);
+    var_num.value = tanInv;
 }
